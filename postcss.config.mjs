eb9e36cda4e76c42ebfa616bc2ac25f273dc3bd4
Marten de Bruijn <martendebruijn@outlook.com>
2025-03-27 20:10:48 +0100

    feat: minify output

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
+import cssnano from 'cssnano'
 import postcssImport from 'postcss-import'
 
 export default {
   plugins: [
-    postcssImport()
+    postcssImport(),
+    cssnano({ preset: 'default' })
   ]
 };
